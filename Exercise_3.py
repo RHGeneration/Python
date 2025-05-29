a1a68147413492cb660cacddfd7101c2a44e22e8
Rafi Haq <RafiH@genstudents.org>
2025-05-29 16:44:42 +0100

    My third exercise

---
+++ Exercise_3.py
@@ -0,0 +1,56 @@
+# 1 kinds of pizzas
+
+pizzas = ("tandoori delight", "texas barbeque", "pineapple")
+
+for pizza in pizzas:
+    print("My favourite pizza is " + pizza + ".")
+
+print(f"I like all of these flavoured pizzas including {pizzas[0]}, {pizzas[1]} and")
+print(f"{pizzas[2]}. The flavours are tasty and unique... Yes I am a pineapple person.")
+
+#===========================================================================================
+
+
+# 2 kinds of animals with similarities
+
+birds = ['Raven', 'Magpie', 'Crow']
+
+for bird in birds:
+    print(bird)
+
+print(f"These birds including the {birds[0]}, {birds[1]} and {birds[2]} have")
+print("common similarities as they all belong in the corvid family")
+print("They are clever birds and are known to mimic other sounds and")
+print("are known as trollsters.")
+
+#===========================================================================================
+
+# 3 Using a loop to print numbers from 1 to 20
+
+numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(numbers)
+
+#===========================================================================================
+
+# 4 Using min() max() functions 
+
+# for value in range(1, 1000000, 10):
+#     print(value)
+
+#===========================================================================================
+
+# 5 Multiples of 3 from 3 to 30
+
+for value in range(3, 30, 3):
+    print(value)
+
+#===========================================================================================
+
+# Stretch challenge
+
+
+
+
+
+
+
